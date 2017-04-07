ff899deb50699fdc3a9ee8bf5783fc1e68710116
Suren <gourapura.2@osu.edu>
2017-04-07 11:20:24 -0400

    Added set_atten function, not complete yet.

--- python_serial_commander.py
+++ python_serial_commander.py
@@ -40,11 +40,12 @@ class Prompt(Cmd):
 			while len(line) >0:
 				print line.rstrip()
 				line = ser.readline()
+
 	def do_allslave_power(self,args):
-		"""Sets the slave power of each channel individually, from slave number 0 to number 3. Format is 'allslave_power [channel 0 on or off: 1 or 0], 			[channel 1 on or off: 1 or 0], [channel 2 on or off: 1 or 0], [channel 3 on or off: 1 or 0]'"""
+		"""Sets the slave power of each channel individually, from slave number 0 to number 3. Format is 'allslave_power [channel 0 on or off: 1 or 0], [channel 1 on or off: 1 or 0], [channel 2 on or off: 1 or 0], [channel 3 on or off: 1 or 0]'"""
 		arguments = args.split(",") #split the arguments up on the comma
 		if len(arguments)!=4:
-			print "Wrong number of arguments! Format is 'allslave_power [channel 0 on or off: 1 or 0], [channel 1 on or off: 1 or 0], [channel 2 on 					or off: 1 or 0], [channel 3 on or off: 1 or 0]"
+			print "Wrong number of arguments! Format is 'allslave_power [channel 0 on or off: 1-0], [channel 1 on or off: 1-0], [channel 2 on or off: 1-0], [channel 3 on or off: 1-0]'"
 		else:
 			slave_setting = [0,0,0,0] #create an array for the four slave power registers, in the order [slave 3, slave 2, slave 1, slave 0]
 			slave_setting[0]=int(arguments[0]) #turn slave 0 on/off
@@ -63,6 +64,60 @@ class Prompt(Cmd):
 				print line.rstrip()
 				line = ser.readline()
 
+	def do_set_atten(self, args):
+		"""Sets the signal or trigger of any channel of any slave with a valid setting. Format is 'set_atten [slave: 0-3], [channel: 0-3], [signal or trigger: 0-1], [setting: 0-127]' """
+		arguments = args.split(",") #split the arguments up on the comma
+		if len(arguments)!=4:
+			print "Wrong number of arguments! Format is 'set_atten [slave: 0-3], [channel: 0-3], [signal or trigger: 0-1], [setting: 0-127]'"
+		elif 0 > int(arguments[0]) or int(arguments[0]) > 3: #only allow correct slave numbers
+			print "Wrong slave number. Slave numbers are 0,1,2, or 3"
+		elif 0 > int(arguments[1]) or int(arguments[1]) > 3: #only allow correct channel numbers
+			print "Wrong channel number. Channel numbers are 0,1,2, or 3"
+		elif 0 != int(arguments[2]) and 1 != int(arguments[2]): #only allow correct sig or trig numbers
+			print "Wrong singal or trigger number. The signal or trigger number is 0 or 1 respectively"
+		elif 0 > int(arguments[3]) or int(arguments[3]) > 127: #only allow correct setting numbers
+			print "Wrong setting number. Setting numbers are whole numbers 0-127"
+		else: 
+			
+			if arguments[2]==0:
+				settingchannel=int(arguments[1])
+			else:
+				settingchannel=4+int(arguments[1])
+			data = '0%d' %(settingchannel)#create the proper command
+			command = 'c05%s!' %(data) #create the command
+			#ser.write(command) #actually write the command to the serial line
+
+			#listen to the output and clear the line
+			#line = ser.readline()
+			#while len(line) >0:
+			#	print line.rstrip()
+			#	line = ser.readline()
+			
+			arg = hex(int(arguments[3])).lstrip('0x') #convert it to hex
+			argument = 'c06%s!' %(arg) #creates the proper argument
+			#ser.write(argument) #actually write the argument to the serial line
+
+			#listen to the output and clear the line
+			#line = ser.readline()
+			#while len(line) >0:
+			#	print line.rstrip()
+			#	line = ser.readline()
+		
+			slaveBinary = "{0:b}".format(int(arguments[0]))
+			print slaveBinary
+			if int(slaveBinary) == 0 or int(slaveBinary) == 1:
+				slaveCtrl = '1000000%s' %(str(slaveBinary))			
+			else:
+				slaveCtrl = '100000%s' %(str(slaveBinary))
+
+			#ser.write(slaveCtrl) #actually write the slaveCtrl to the serial line
+
+			#listen to the output and clear the line
+			#line = ser.readline()
+			#while len(line) >0:
+			#	print line.rstrip()
+			#	line = ser.readline()
+			
 def start_serial(try_port):                                         
 	print "got inside start_serial"
 	try:
